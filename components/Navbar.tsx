5c3f9bf63aef4ff8c0502aea711d16636adf0610
awbibib <awbibib@gmail.com>
2024-03-02 20:43:03 +0700

    tweaking navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,24 +6,15 @@ import { useState, useEffect, useContext } from 'react';
 
 const Navbar = () => {
   const { changeTheme }: any = useContext(ThemeContext);
-  const [isSunset, setIsSunset] = useState(false);
   const [sunnySize, setSunnySize] = useState(28);
   const [moonSize, setMoonSize] = useState(28);
-
-  // Function to handle theme change
-  const handleChangeTheme = () => {
-    const newTheme = !isSunset ? 'sunset' : 'nord';
-    changeTheme(newTheme);
-    setIsSunset(prevState => !prevState);
-    // Store theme preference in local storage
-    localStorage.setItem('theme', newTheme);
-  };
 
   useEffect(() => {
     // Retrieve theme preference from local storage on component mount
     const storedTheme = localStorage.getItem('theme');
-    setIsSunset(storedTheme === 'sunset');
-
+    const isSunset = storedTheme === 'sunset';
+    // Update theme based on the retrieved preference
+    changeTheme(storedTheme);
     // Adjust icon sizes based on the theme
     if (isSunset) {
       setSunnySize(28);
@@ -32,8 +23,24 @@ const Navbar = () => {
       setSunnySize(34);
       setMoonSize(28);
     }
-  }, [isSunset]);
+  }, []);
 
+  // Function to handle theme change
+  const handleChangeTheme = () => {
+    const newTheme = localStorage.getItem('theme') === 'sunset' ? 'nord' : 'sunset';
+    // Update theme preference in local storage
+    localStorage.setItem('theme', newTheme);
+    // Update theme
+    changeTheme(newTheme);
+    // Adjust icon sizes based on the new theme
+    if (newTheme === 'sunset') {
+      setSunnySize(28);
+      setMoonSize(34);
+    } else {
+      setSunnySize(34);
+      setMoonSize(28);
+    }
+  };
 
   return (
     <div className="navbar bg-neutral px-4">
@@ -42,7 +49,7 @@ const Navbar = () => {
       </div>
       <div className="flex-none">
         <IoIosSunny size={sunnySize} color='#FFA500' className='ease-in-out duration-300' />
-        <input type="checkbox" className="toggle mx-1 rounded-xl" checked={isSunset} onChange={handleChangeTheme} />
+        <input type="checkbox" className="toggle mx-1 rounded-xl" checked={localStorage.getItem('theme') === 'sunset'} onChange={handleChangeTheme} />
         <IoIosMoon size={moonSize} className='ease-in-out duration-300' />
       </div>
     </div>
